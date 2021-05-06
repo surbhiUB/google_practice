c06519632ec30dc21e48c308c78587a05c419356
Surbhi Bhatnagar <surbhibh@buffalo.edu>
2021-05-06 00:28:59 -0700

    Create findMediamDataStream.py

---
+++ findMediamDataStream.py
@@ -0,0 +1,71 @@
+"""
+295. Find Median from Data Stream
+Hard
+
+4116
+
+78
+
+Add to List
+
+Share
+The median is the middle value in an ordered integer list. If the size of the list is even, there is no middle value and the median is the mean of the two middle values.
+
+For example, for arr = [2,3,4], the median is 3.
+For example, for arr = [2,3], the median is (2 + 3) / 2 = 2.5.
+Implement the MedianFinder class:
+
+MedianFinder() initializes the MedianFinder object.
+void addNum(int num) adds the integer num from the data stream to the data structure.
+double findMedian() returns the median of all elements so far. Answers within 10-5 of the actual answer will be accepted.
+ 
+
+Example 1:
+
+Input
+["MedianFinder", "addNum", "addNum", "findMedian", "addNum", "findMedian"]
+[[], [1], [2], [], [3], []]
+Output
+[null, null, null, 1.5, null, 2.0]
+
+Explanation
+MedianFinder medianFinder = new MedianFinder();
+medianFinder.addNum(1);    // arr = [1]
+medianFinder.addNum(2);    // arr = [1, 2]
+medianFinder.findMedian(); // return 1.5 (i.e., (1 + 2) / 2)
+medianFinder.addNum(3);    // arr[1, 2, 3]
+medianFinder.findMedian(); // return 2.0
+ 
+
+Constraints:
+
+-105 <= num <= 105
+There will be at least one element in the data structure before calling findMedian.
+At most 5 * 104 calls will be made to addNum and findMedian.
+ 
+
+Follow up:
+
+If all integer numbers from the stream are in the range [0, 100], how would you optimize your solution?
+If 99% of all integer numbers from the stream are in the range [0, 100], how would you optimize your solution?
+"""
+
+from heapq import *
+class MedianFinder:
+
+    def __init__(self):
+        self.heaps = [],[]
+
+    def addNum(self, num):
+        #small is max-heap
+        #large is min heap
+        small, large = self.heaps
+        heappush(small, -heappushpop(large,num))
+        if len(large)<len(small):
+            heappush(large,-heappop(small))
+
+    def findMedian(self):
+        small,large = self.heaps
+        if len(large)>len(small):
+            return float(large[0])
+        return (large[0]-small[0])/2.0
